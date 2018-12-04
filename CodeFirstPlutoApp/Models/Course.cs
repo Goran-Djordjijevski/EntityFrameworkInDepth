621707b84304964ef9f2b7a74109049f8e13bf88
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-12-04 22:08:43 +0100

    Modifying Existing model Course

--- CodeFirstPlutoApp/Models/Course.cs
+++ CodeFirstPlutoApp/Models/Course.cs
@@ -10,7 +10,7 @@ namespace CodeFirstPlutoApp.Models
     {
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string Name { get; set; }
 
         public string Description { get; set; }
 
