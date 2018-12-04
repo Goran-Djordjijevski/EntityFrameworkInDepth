8b6449336ce3ff223dc0985db7d25d2233409b56
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-12-04 20:56:33 +0100

    Change contructor place

--- CodeFirstPlutoApp/Models/PlutoContext.cs
+++ CodeFirstPlutoApp/Models/PlutoContext.cs
@@ -9,15 +9,15 @@ namespace CodeFirstPlutoApp.Models
 {
     public class PlutoContext : DbContext
     {
+        public PlutoContext() : base(nameOrConnectionString: "name=DefaultConnection")
+        {
+
+        }
+
         public DbSet<Course> Courses { get; set; }
 
         public DbSet<Author> Authors { get; set; }
 
         public DbSet<Tag> Tags { get; set; }
-
-        public PlutoContext() : base(nameOrConnectionString: "name=DefaultConnection")
-        {
-
-        }
     }
 }
