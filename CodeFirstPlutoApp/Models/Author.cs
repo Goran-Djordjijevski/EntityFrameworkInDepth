f85ce309dd63a1a3ff9cc81dbd468f46773bec48
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-12-04 20:00:31 +0100

    Created new project.

---
+++ CodeFirstPlutoApp/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstPlutoApp.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        // Navigation properties
+        public IList<Course> Courses { get; set; }
+    }
+}
---
+++ CodeFirstPlutoApp/Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstPlutoApp.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public CourseLevel Level { get; set; }
+
+        public float FullPrice { get; set; }
+
+        // Navigation properties
+        public Author Author { get; set; }
+
+        public IList<Tag> Tags { get; set; }
+    }
+}
